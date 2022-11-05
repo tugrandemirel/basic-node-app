bee6ce6c796504e9bb3a1da075b4b5cb06cc131a
Tugran Demirel <demireltugran66@gmail.com>
2022-11-06 01:56:54 +0300

    admin controller düzenlenmesi

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,29 +1,39 @@
-const Admin = require('../models/product')
+const Product = require('../models/product')
 
-// module.exports.getProducts = (req, res, next) =>{
-//    const products = Admin.getAll();
-//     res.render('index', {
-//        title: 'Home Page',
-//        products: products,
-//        path: '/'
-//     })
-//  }
-//
-//  exports.getAddProduct = (req, res, next) =>{
-//     console.log('middleware çalıştı')
-//     res.render('add-product', {
-//         title: 'Add a new product',
-//         path: '/admin/add-product'
-//     });
-// }
-//
-// exports.postAddProduct = (req, res, next) => {
-//     const product = new Admin(
-//         req.body.name,
-//         req.body.price,
-//         req.body.imageUrl,
-//         req.body.description
-//     )
-//     product.saveProduct();
-//     res.redirect('/')
-// }
+module.exports.getProducts = (req, res, next) =>{
+   const products = Product.getAll();
+    res.render('admin/products', {
+       title: 'Admin Products',
+       products: products,
+       path: '/admin/products'
+    })
+ }
+
+ exports.getAddProduct = (req, res, next) =>{
+    res.render('admin/add-product', {
+        title: 'Add a new product',
+        path: '/admin/add-product'
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(
+        req.body.name,
+        req.body.price,
+        req.body.imageUrl,
+        req.body.description
+    )
+    product.saveProduct();
+    res.redirect('/')
+}
+
+ exports.getEditProduct = (req, res, next) =>{
+    res.render('admin/edit-product', {
+        title: 'Edit Product',
+        path: '/admin/edit-product'
+    });
+}
+
+exports.postEditProduct = (req, res, next) => {
+    res.redirect('/')
+}
