c11d77c0574a2f79cb0d5111cf9a57f193514725
Tugran Demirel <demireltugran66@gmail.com>
2022-11-07 02:52:58 +0300

    product ile categories arasındaki ilişkinin kurulması

--- app.js
+++ app.js
@@ -14,16 +14,25 @@ const userRoutes = require('./routes/shop');
 const errorController = require('./controllers/errors')
 const sequelize = require('./utility/database');
 
+const Category = require('./models/category');
+const Product = require('./models/product');
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 // routes
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
-
-
 app.use(errorController.get404Page)
 
-sequelize.sync()
+// Product.hasOne(Category);
+Product.belongsTo(Category, {
+    foreignKey: {
+        allowNull: false
+    }
+});
+Category.hasMany(Product);
+sequelize
+    .sync({force: true})
     .then(result => {
         console.log(result)
     })
