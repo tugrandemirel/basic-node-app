3231435935a24ff691faf1f74d92323550006ae2
Tugran Demirel <demireltugran66@gmail.com>
2022-11-07 18:01:14 +0300

    user table oluşturulması ve product table ile ilişkilendirilmesi

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const sequelize = require('./utility/database');
 
 const Category = require('./models/category');
 const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -25,32 +26,44 @@ app.use(userRoutes);
 app.use(errorController.get404Page)
 
 // Product.hasOne(Category);
-Product.belongsTo(Category, {
-    foreignKey: {
-        allowNull: false
-    }
-});
+Product.belongsTo(Category, {foreignKey: {allowNull: false}});
 Category.hasMany(Product);
+
+Product.belongsTo(User);
+User.hasMany(Product);
+
+
+
 sequelize
     // .sync({force: true})
     .sync()
     .then(result => {
-        Category.count()
-            .then(count =>{
-                if (count === 0) {
-                    Category.bulkCreate([
-                        {name: 'Telefon', description: 'Telefon kategorisi'},
-                        {name: 'Bilgisayar', description: 'Bilgisayar kategorisi'},
-                        {name: 'Elektronik', description: 'Elektronik kategorisi'}
-                    ])
+        User.findByPk(1)
+            .then((user) => {
+                if(!user){
+                    User.create({
+                        name: 'Ahmet',
+                        email: 'deneme@deneme.com'
+                    })
                 }
+                return user;
             })
-
-        console.log(result)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+            .then((user) => {
+                Category.count()
+                    .then(count =>{
+                        if (count === 0) {
+                            Category.bulkCreate([
+                                {name: 'Telefon', description: 'Telefon kategorisi'},
+                                {name: 'Bilgisayar', description: 'Bilgisayar kategorisi'},
+                                {name: 'Elektronik', description: 'Elektronik kategorisi'}
+                            ])
+                        }
+                    })
+            })
+            })
+            .catch(err => {
+                console.log(err)
+            })
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
 });
