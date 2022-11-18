8c4630d508473c536b3b605e2f26afab599ece9e
Tugran Demirel <demireltugran66@gmail.com>
2022-11-19 01:11:03 +0300

    login validation

--- controllers/account.js
+++ controllers/account.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Login = require('../models/login');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 exports.getLogin = (req, res, next) => {
@@ -15,59 +16,67 @@ exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ email: email })
-        .then(user => {
-            if(!user){
-                req.session.errorMessage = 'Bu mail adresi ile bir kullanıcı bulunamadı';
-                req.session.save(function (err) {
-                    console.log(err);
-                    return res.redirect('/login');
-                })
-            }
-            // hashalenmiş passwrod ile kullanıcıdan gelen password karşılaştırması
-            bcrypt.compare(password, user.password)
-                .then(isSuccess => {
-                    if(isSuccess){
-                        // login
-                        // kullanıcı session'a kaydedilir
-                        req.session.isAuthentication = true;
-                        // session'a kaydedilen kullanıcı bilgileri
-                        req.session.user = user;
-                        return req.session.save(function (err) {
-                            var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/';
-                            // session'dan redirect url silinir
-                            delete req.session.redirectTo;
-                            res.redirect(redirectTo);
-                        })
-                    }else{
-                        // not login
-                        req.session.errorMessage = 'Şifre hatalı';
+    const loginModel = new Login({
+        email: email,
+        password: password
+    })
+    loginModel.validate()
+        .then(() => {
+            User.findOne({ email: email })
+                .then(user => {
+                    if(!user){
+                        req.session.errorMessage = 'Bu mail adresi ile bir kullanıcı bulunamadı';
                         req.session.save(function (err) {
                             console.log(err);
                             return res.redirect('/login');
                         })
                     }
+                    // hashalenmiş passwrod ile kullanıcıdan gelen password karşılaştırması
+                    bcrypt.compare(password, user.password)
+                        .then(isSuccess => {
+                            if(isSuccess){
+                                // login
+                                // kullanıcı session'a kaydedilir
+                                req.session.isAuthentication = true;
+                                // session'a kaydedilen kullanıcı bilgileri
+                                req.session.user = user;
+                                return req.session.save(function (err) {
+                                    var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/';
+                                    // session'dan redirect url silinir
+                                    delete req.session.redirectTo;
+                                    res.redirect(redirectTo);
+                                })
+                            }else{
+                                // not login
+                                req.session.errorMessage = 'Şifre ya da email adresi hatalı';
+                                req.session.save(function (err) {
+                                    console.log(err);
+                                    return res.redirect('/login');
+                                })
+                            }
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
                 })
                 .catch(err => {
                     console.log(err);
                 })
         })
         .catch(err => {
-            console.log(err);
+            if (err.name === 'ValidationError') {
+                var message = '';
+                for (field in err.errors) {
+                    message += err.errors[field].message + '<br>';
+                }
+                res.render('account/login', {
+                    path: '/login',
+                    title: 'Login',
+                    errorMessage: message,
+                    csrfToken: req.csrfToken()
+                })
+            }
         })
-
-   /* if (email === 'demireltugran66@gmail.com' && password === '123456') {
-        //req.isAuthenticated = true;
-        // cookie => kullanıcı tarayıcısında bilgiler saklanır.
-        // res.cookie('isAuthentication', true);
-
-        // session => kullanıcı bilgileri sunucuda saklanır.
-        req.session.isAuthentication = true;
-        res.redirect('/');
-    }else{
-        req.isAuthenticated = false;
-        res.redirect('/login');
-    }*/
 }
 
 exports.getRegister = (req, res, next) => {
